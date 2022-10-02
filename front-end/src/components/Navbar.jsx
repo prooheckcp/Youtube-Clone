fa0288a23e04fb15315baeba61a5d9494922de6c
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 20:11:43 +0100

    Creating search bar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
-
+import { SearchBar } from '../components';
 import { logo } from '../utils/constants';
 
 const Navbar = () => (
@@ -18,7 +18,7 @@ const Navbar = () => (
         <Link to="/" style={{display:'flex', alignItems:'center'}}>
           <img src={logo} alt="logo" height={45}/>
         </Link>
-        
+        <SearchBar />
   </Stack>
 )
 
