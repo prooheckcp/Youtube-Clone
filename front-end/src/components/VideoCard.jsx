e2183c3a16de4ad5a96110c85c58cc19924a2247
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 21:16:45 +0100

    Showing our card video

--- front-end/src/components/VideoCard.jsx
+++ front-end/src/components/VideoCard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const VideoCard = () => {
+import {Link} from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTyitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
+
+const VideoCard = ({video:{id:{videoId}, snippet}}) => {
+
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}`: demoVideoUrl}>
+        <CardMedia 
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{width: 358, height: 180}}
+        />
+      </Link>
+    </Card>
   )
 }
 
