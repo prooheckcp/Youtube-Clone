e0f5afd22fc47c7144cdee96e97387a8321567f1
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 21:58:57 +0100

    Fetching channel data

--- front-end/src/components/ChannelDetail.jsx
+++ front-end/src/components/ChannelDetail.jsx
@@ -6,14 +6,20 @@ import {Videos, ChannelCard} from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  const [channelDetail, setChannelDetail] = useState(null)
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
   const {id} = useParams();
 
-  console.log(channelDetail)
+  console.log(channelDetail, videos)
 
   useEffect(()=>{
-    fetchFromAPI(`channels?part="snippet&id=${id}`).then(data=>{
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then(data=>{
       setChannelDetail(data?.items[0]);
+    })
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(data=>{
+      setVideos(data?.items);
     })
   }, [id])
 
