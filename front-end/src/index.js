d48c06305ff391c376bf401e36e707ad3aff5af3
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 19:51:07 +0100

    Creating router structure

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'; 
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './index.css';
 
-const ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App/>);
