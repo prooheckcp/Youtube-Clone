1d65ea984efde51f22e83256306852a33a87588a
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 20:30:39 +0100

    Adding side buttons

--- front-end/src/components/SideBar.jsx
+++ front-end/src/components/SideBar.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
+
+const selected = "new";
 
 const SideBar = () => {
   return (
-    <div>Sidebar</div>
+    <Stack
+        sx={{
+            overflowY:"auto",
+            height: {sx: 'auto', md: '95%'},
+            flexDirection: {md: 'column'},
+        }}
+    >
+        {categories.map((category)=>(
+            <button
+                className="category-btn"
+                style={{
+                    background: category.name === selected && '#FC1503',
+                    color: 'white'
+                }}
+            >
+                <span>{category.icon}</span>
+                <span>{category.name}</span>
+            </button>
+        ))}
+    </Stack>
   )
 }
 
