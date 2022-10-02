6f6f1d9d0b84e9dd0c53a2211d87131e3d8c06fd
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 20:25:18 +0100

    Creating feed structure

--- front-end/src/components/Feed.jsx
+++ front-end/src/components/Feed.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import {Box, Stack, Typography} from '@mui/material';
 
 const Feed = () => {
+  const currentDate = new Date();
+
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{flexDirection: {sx:"column", md: "row"}}}
+    >
+      <Box
+        sx={{height:{sx:'auto', md:'92vh'}, borderRight:'apx solid #3d3d3d', px: {sx: 0, md: 2}}}
+      >
+        SideBar
+        <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
+          {`Copyright ${currentDate.getFullYear()} Prooheckcp`}
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
