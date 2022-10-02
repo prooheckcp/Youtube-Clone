dc4c441cb07f9e9176195d40507d07f6924ab021
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 20:36:43 +0100

    Creating seperate videos

--- front-end/src/components/Feed.jsx
+++ front-end/src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 import {Box, Stack, Typography} from '@mui/material';
-import {SideBar} from '../components';
+import {SideBar, Videos} from '../components';
 
 const Feed = () => {
   const currentDate = new Date();
@@ -16,6 +16,18 @@ const Feed = () => {
         <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
           {`Copyright ${currentDate.getFullYear()} Prooheckcp`}
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{
+        overflowY:'auto',
+        height:'90vh', 
+        flex:2
+      }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{color:"white"}}>
+          New <span style={{color: '#F31503'}}>videos</span>
+        </Typography>
+
+        <Videos/>
       </Box>
     </Stack>
   )
