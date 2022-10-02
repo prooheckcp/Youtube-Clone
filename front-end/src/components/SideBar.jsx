8ff363880c5336a1b314ea60770fc3d266448f47
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 20:32:49 +0100

    Styling the sidebar

--- front-end/src/components/SideBar.jsx
+++ front-end/src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selected = "new";
+const selectedCategory = "New";
 
 const SideBar = () => {
   return (
@@ -17,12 +17,12 @@ const SideBar = () => {
             <button
                 className="category-btn"
                 style={{
-                    background: category.name === selected && '#FC1503',
+                    background: category.name === selectedCategory && '#FC1503',
                     color: 'white'
                 }}
             >
-                <span>{category.icon}</span>
-                <span>{category.name}</span>
+                <span style={{color:category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+                <span style={{opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
             </button>
         ))}
     </Stack>
