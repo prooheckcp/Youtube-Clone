03726d5a9aa29d442c8c0f1ccf9d6f31da19f4ef
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-02 21:02:49 +0100

    Fixing API errors

--- front-end/src/components/Feed.jsx
+++ front-end/src/components/Feed.jsx
@@ -11,11 +11,9 @@ const Feed = () => {
   const [videos, setVideos] = useState([]);
 
   useEffect(()=>{
-    /*
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=>{
       setVideos(data.items);
-    })    
-    */
+    });
 
   }, [setSelectedCategory])
 
